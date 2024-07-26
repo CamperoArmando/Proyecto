6a9ab7a40871961f72956eceff89a369e6df7e9a
CamperoArmando <camperoarmando@micorreo.upp.edu.mx>
2024-07-26 10:53:04 -0600

    Modificacion IngredienteController

--- Proyecto/routes/web.php
+++ Proyecto/routes/web.php
@@ -1,16 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\IngredientesController;
+use App\Http\Controllers\IngredienteController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 // Ruta para mostrar los ingredientes
-Route::get('/ingredientes', [IngredientesController::class, 'index'])->name('ingredientes.index');
+Route::get('/ingredientes', [IngredienteController::class, 'index'])->name('ingredientes.index');
 
 // Ruta para editar un ingrediente
-Route::get('/ingredientes/{id}/edit', [IngredientesController::class, 'edit'])->name('ingredientes.edit');
+Route::get('/ingredientes/{id}/edit', [IngredienteController::class, 'edit'])->name('ingredientes.edit');
 
 // Ruta para actualizar un ingrediente
-Route::put('/ingredientes/{id}', [IngredientesController::class, 'update'])->name('ingredientes.update');
+Route::put('/ingredientes/{id}', [IngredienteController::class, 'update'])->name('ingredientes.update');
