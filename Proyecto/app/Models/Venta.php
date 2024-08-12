54d8bf01fe853e1a150b4a1e7dde7b07bda2ccbb
CamperoArmando <camperoarmando@micorreo.upp.edu.mx>
2024-08-12 00:49:48 -0600

    Venta

--- Proyecto/app/Models/Venta.php
+++ Proyecto/app/Models/Venta.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Venta extends Model
 {
     use HasFactory;
+
+    // Especifica los campos que pueden ser asignados masivamente
+    protected $fillable = [
+        'nombre_del_plato',
+        'precio',
+        // Otros campos permitidos
+    ];
 }
